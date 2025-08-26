4183a26f2c52964f09125341e123102bf80245ca
Mylena Viana Nunes <126629062+mylenosa@users.noreply.github.com>
2025-08-26 14:43:34 -0400

    feat: Adiciona o Google Analytics

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -20,7 +20,7 @@ export const config = {
     keywords: 'busca local, produtos, Ariquemes, WhatsApp, lojas físicas',
   },
   analytics: {
-    gtag: 'GA_MEASUREMENT_ID', // Substituir pelo ID real
+    gtag: 'G-VYMZFE3EB6', // Substituir pelo ID real
     fbPixel: 'FB_PIXEL_ID', // Substituir pelo ID real
   }
 } as const;
