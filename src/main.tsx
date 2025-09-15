cfad04fc9d85cfd73949d7d78f4564efd99e4307
mylenosa <mylenavnunes2004@gmail.com>
2025-09-15 15:29:41 -0400

    ajeitando para o supabase v13

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { router } from './router';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
